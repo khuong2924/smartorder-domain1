d644806dfbcf195858d9ae04d76dd72a8b4e8ffa
khuong2924 <truongkhuong666@gmail.com>
2025-01-16 11:48:12 +0700

    add jwt & config login

---
+++ src/main/java/khuong/com/smartorder/config/JwtConfig.java
@@ -0,0 +1,29 @@
+package khuong.com.smartorder.config;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.security.Key;
+
+@Configuration
+public class JwtConfig {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Bean
+    public Key key() {
+        return Keys.hmacShaKeyFor(java.util.Base64.getDecoder().decode(jwtSecret));
+    }
+
+    public String generateToken(String subject) {
+        return Jwts.builder()
+                .setSubject(subject)
+                .signWith(key(), SignatureAlgorithm.HS512)
+                .compact();
+    }
+}
